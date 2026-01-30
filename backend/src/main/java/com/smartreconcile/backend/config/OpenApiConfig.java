f9672fb30241d16adfe674e9e24380d512218f63
omkarrathod23 <omkarrathod101050@gmail.com>
2026-01-31 00:02:06 +0530

    Fix Swagger authorize button

---
+++ backend/src/main/java/com/smartreconcile/backend/config/OpenApiConfig.java
@@ -0,0 +1,13 @@
+package com.smartreconcile.backend.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
+@OpenAPIDefinition(security = @SecurityRequirement(name = "bearerAuth"))
+public class OpenApiConfig {
+}
